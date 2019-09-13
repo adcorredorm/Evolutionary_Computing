d82967e80de3861ca46a5b215423fa90493584ac
Angel Corredor <adcorredorm@unal.edu.co>
2019-09-13 00:03:27 -0500

    styling output

--- SinglePoint/experiment.py
+++ SinglePoint/experiment.py
@@ -14,10 +14,11 @@ stopp = lambda k, vector, function: k >= iterations/theads
 function = opt_functions.ackley
 _min = -32
 _max = 32
+dim = 12
 
-hc  = HillClimb(10, function, stop)
-hcp = ParallelHC(theads, 10, function, stopp)
-st  = SimulatedAnnealing(10, function, stop)
+hc  = HillClimb(dim, function, stop)
+hcp = ParallelHC(theads, dim, function, stopp)
+st  = SimulatedAnnealing(dim, function, stop)
 
 hc_candidates  = []
 hcp_candidates = []
@@ -27,7 +28,7 @@ for i in range(executions):
   hc_candidates.append(hc.execute(_min, _max))
   hcp_candidates.append(hcp.execute(_min, _max))
   st_candidates.append(st.execute(_min, _max))
-  print(i)
+  print((i+1)+'%')
 
 hc_results  = [function(cand) for cand in hc_candidates]
 hcp_results = [function(cand) for cand in hcp_candidates]
@@ -37,39 +38,44 @@ min_hc_index = hc_results.index(min(hc_results))
 min_hcp_index = hcp_results.index(min(hcp_results))
 min_st_index = st_results.index(min(st_results))
 
-
-print("HC")
-av = sum(hc_results)/executions
-print("Av:", av)
-print("DS:", math.sqrt(sum([(res - av)**2 for res in hc_results])/executions))
+hc_av = sum(hc_results)/executions
 hc_results.sort()
-med = hc_results[int(executions/2)]
-print("Med:", med)
-print("DS:", math.sqrt(sum([(res - med)**2 for res in hc_results])/executions))
-print("Max:", hc_results[-1])
-print("Min:", hc_results[0])
-print(hc_candidates[min_hc_index])
+hc_med = hc_results[int(executions/2)]
 
-print("HCP")
-av = sum(hcp_results)/executions
-print("Av:", av)
-print("DS:", math.sqrt(sum([(res - av)**2 for res in hcp_results])/executions))
+hcp_av = sum(hcp_results)/executions
 hcp_results.sort()
-med = hcp_results[int(executions/2)]
-print("Med:", med)
-print("DS:", math.sqrt(sum([(res - med)**2 for res in hcp_results])/executions))
-print("Max:", hcp_results[-1])
-print("Min:", hcp_results[0])
-print(hcp_candidates[min_hcp_index])
+hcp_med = hcp_results[int(executions/2)]
 
-print("ST")
-av = sum(st_results)/executions
-print("Av:", av)
-print("DS:", math.sqrt(sum([(res - av)**2 for res in st_results])/executions))
+st_av = sum(st_results)/executions
 st_results.sort()
-med = st_results[int(executions/2)]
-print("Med:", med)
-print("DS:", math.sqrt(sum([(res - med)**2 for res in st_results])/executions))
-print("Max:", st_results[-1])
-print("Min:", st_results[0])
-print(st_candidates[min_st_index])
+st_med = st_results[int(executions/2)]
+
+name = function.__name__
+file = open(name + '_results.txt', 'w+')
+
+file.write('Params:\n\tFunction: {}\n\tDimension: {:.0f}\n\tIterations: {:.0f}\n\tExecutions: {:.0f}\n\n'.format(
+  name, dim, iterations, executions
+))
+
+file.write('Algorithm    \t    Average    \t\t    Median    \t\t  Max\t\t  Min\t\tBest Individual\n')
+
+file.write('Hill Climb   \t{:0.4f} ± {:0.4f}\t{:0.4f} ± {:0.4f}\t{:0.4f}\t{:0.4f}\t'.format(
+  hc_av, math.sqrt(sum([(res - hc_av)**2 for res in hc_results])/executions),
+  hc_results[int(executions/2)], math.sqrt(sum([(res - hc_med)**2 for res in hc_results])/executions),
+  hc_results[-1], hc_results[0]
+))
+file.write(str(['{:0.4f}'.format(i) for i in hc_candidates[min_hc_index]]) + '\n')
+
+file.write('Parallel HC  \t {:0.4f} ± {:0.4f} \t {:0.4f} ± {:0.4f} \t{:0.4f}\t{:0.4f}\t'.format(
+  hcp_av, math.sqrt(sum([(res - hcp_av)**2 for res in hcp_results])/executions),
+  hcp_results[int(executions/2)], math.sqrt(sum([(res - hcp_med)**2 for res in hcp_results])/executions),
+  hcp_results[-1], hcp_results[0]
+))
+file.write(str(['{:0.4f}'.format(i) for i in hcp_candidates[min_hcp_index]]) + '\n')
+
+file.write('Sim. Anneling\t{:0.4f} ± {:0.4f}\t{:0.4f} ± {:0.4f}\t{:0.4f}\t{:0.4f}\t'.format(
+  st_av, math.sqrt(sum([(res - st_av)**2 for res in st_results])/executions),
+  st_results[int(executions/2)], math.sqrt(sum([(res - st_med)**2 for res in st_results])/executions),
+  st_results[-1], st_results[0]
+))
+file.write(str(['{:0.4f}'.format(i) for i in st_candidates[min_st_index]]) + '\n')
