a00ea273cbf8123b7b0cd3b2cadcc5c54ae3007c
Angel Corredor <adcorredorm@unal.edu.co>
2019-10-02 18:50:48 -0500

    comparators updated

--- PoblationalSearch/Agents/Agent.py
+++ PoblationalSearch/Agents/Agent.py
@@ -30,13 +30,17 @@ class Agent(metaclass=ABCMeta):
 	def __lt__(self, other):
 		return isinstance(other, self.__class__) and self.fitness < other.fitness
 
-	def __le__(self, other): return self < other or self == other
+	def __le__(self, other): 
+		return isinstance(other, self.__class__) and self.fitness <= other.fitness
 	
 	def __eq__(self, other):
 		return isinstance(other, self.__class__) and self.fitness == other.fitness
 
-	def __ne__(self, other): return not self == other
+	def __ne__(self, other): 
+		return isinstance(other, self.__class__) and self.fitness != other.fitness
 
-	def __ge__(self, other): return not self < other
+	def __ge__(self, other): 
+		return isinstance(other, self.__class__) and self.fitness >= other.fitness
 
-	def __gt__(self, other): return not self <= other
+	def __gt__(self, other): 
+		return isinstance(other, self.__class__) and self.fitness > other.fitness
