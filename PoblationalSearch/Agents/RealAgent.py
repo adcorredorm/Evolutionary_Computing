cc1e1b7fd1d5eec03f960513508a87357157bad4
Angel Corredor <adcorredorm@unal.edu.co>
2019-10-15 09:45:57 -0500

    RealAgent modified to include exogenous params

--- PoblationalSearch/Agents/RealAgent.py
+++ PoblationalSearch/Agents/RealAgent.py
@@ -1,21 +1,32 @@
 from random import random, gauss
-from Agents.Agent import Agent
+from .Agent import Agent
 
 class RealAgent(Agent):
 
-	def init(self, size, _min=0, _max=1):
+	def init(self, size, _min=0, _max=1, exogenous=False):
 		self.genome = [random()*(_max - _min) + _min for _ in range(size)]
+		if exogenous:
+			self.exogenous = self.init_exogenous()
 
 	def evaluate(self, function):
 		if self.fitness is None:
 			self.fitness = function(self.genome)
 
 	def mutate(self, sigma=0.1, rate=-1):
-		rate = rate if rate >= 0 else 1/len(self.genome)
+		if rate < 0:
+			rate = 1/len(self.genome)
 		for i in range(len(self.genome)):
 			if random() < rate:
 				self.genome[i] = gauss(self.genome[i], sigma)
-
+		
+	def init_exogenous(self):
+		return [random() for _ in range(len(self.genome))]
+	
+	def mutate_exogenous(self):
+		self.exogenous = [gauss(ex, ex) for ex in self.exogenous]
+		for i in range(len(self.genome)):
+			self.genome[i] = gauss(self.genome[i], self.exogenous[i])
+		
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 
